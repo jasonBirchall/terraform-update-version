1368181feea06bbc5ef4d26c7b2fafdbfcf461c4
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2021-02-10 17:33:30 +0000

    Refactor alot of the main.go to allow for multiple steps

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ package main
 import (
 	"bufio"
 	"context"
-	"fmt"
 	"log"
 	"os"
 
+	execute "github.com/alexellis/go-execute/pkg/v1"
 	git "github.com/go-git/go-git/v5"
-	// . "github.com/go-git/go-git/v5/_examples"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/google/go-github/github"
 )
@@ -42,8 +42,9 @@ var url = "https://github.com/ministryofjustice/"
 
 func main() {
 	token := os.Getenv("GITHUB_AUTH_TOKEN")
-	if token == "" {
-		log.Fatal("Unauthorised: No token present")
+	user := os.Getenv("GITHUB_AUTH_USER")
+	if token == "" || user == "" {
+		log.Fatal("Unauthorised: No user or token present")
 	}
 
 	repos, err := getRepos()
@@ -52,42 +53,90 @@ func main() {
 	}
 
 	for _, repo := range repos {
-		r, err := git.PlainClone(repo, true, &git.CloneOptions{
-			// The intended use of a GitHub personal access token is in replace of your password
-			// because access tokens can easily be revoked.
-			// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
-			Auth: &http.BasicAuth{
-				Username: "jasonBirchall", // yes, this can be anything except an empty string
-				Password: token,
-			},
-			URL:      url + repo,
-			Progress: os.Stdout,
-		})
+		err := cloneRepo(repo, token, user)
 		if err != nil {
-			log.Fatalf("Unable to clone repo: %s\n", err)
+			log.Fatalf("Unable to clone repository: %s\n", err)
 		}
-		fmt.Println(r)
 
+		err = executeCommand(repo)
+		if err != nil {
+			log.Fatalf("Unable to execute command: %s\n", err)
+		}
+	}
+}
+
+func executeCommand(repo string) error {
+	cmd := execute.ExecTask{
+		Command:     "terraform",
+		Args:        []string{"0.13upgrade", "--yes"},
+		StreamStdio: false,
 	}
 
-	// 	ref, err := r.Head()
-	// 	if err != nil {
-	// 		log.Fatalf("Unable to retrieve branch: %s\n", err)
-	// 	}
+	os.Chdir(repo)
+	_, err := os.Getwd()
+	if err != nil {
+		return err
+	}
 
-	// 	commit, err := r.CommitObject(ref.Hash())
-	// 	if err != nil {
-	// 		log.Fatalf("Unable to retrieve commit object: %s\n", err)
-	// 	}
-	// 	fmt.Println(commit)
+	_, err = cmd.Execute()
+	if err != nil {
+		return err
+	}
 
-	// 	// run command
-	// 	// commit
-	// 	// pull request
-	// 	// add link to pr to collection and print
-	// }
+	os.Chdir("..")
+	_, err = os.Getwd()
+	if err != nil {
+		return err
+	}
 
+	return nil
 }
+
+func cloneRepo(repo, token, user string) error {
+	r, err := git.PlainClone(repo, false, &git.CloneOptions{
+		Auth: &http.BasicAuth{
+			Username: user, // yes, this can be anything except an empty string
+			Password: token,
+		},
+		URL: url + repo,
+		// Progress: os.Stdout,
+	})
+	if err != nil {
+		return err
+	}
+
+	headRef, err := r.Head()
+	if err != nil {
+		return err
+	}
+
+	ref := plumbing.NewHashReference("refs/heads/tf-0.13upgrade", headRef.Hash())
+
+	err = r.Storer.SetReference(ref)
+	if err != nil {
+		return err
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{
+		Hash: plumbing.NewHash(ref),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// 	// run command
+// 	// commit
+// 	// pull request
+// 	// add link to pr to collection and print
+// }
 
 func getRepos() ([]string, error) {
 	var s []string
